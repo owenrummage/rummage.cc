c9d78bb3cf63e84544b13ac48e1b8c5257ad3a77
Owen Rummage <owen@rummage.cc>
2024-02-28 09:42:16 -0600

    Add blog functions using MDX parser

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-      nesting: true,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+    nesting: true
+  }), mdx()],
   output: "server",
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
